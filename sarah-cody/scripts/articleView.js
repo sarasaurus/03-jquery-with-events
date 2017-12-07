68d831dadbffc2fc6ac05859330f35dbdb7640bf
Sarah Bixler <bixler.s.e@gmail.com>
2017-12-07 13:20:15 -0800

    cleared out comments and added a statement to hide all .template class in the articleView.js

--- sarah-cody/scripts/articleView.js
+++ sarah-cody/scripts/articleView.js
@@ -1,30 +1,20 @@
 'use strict';
 
-// REVIEW: Configure an object to hold all of our functions for dynamic updates and article-related event handlers.
 let articleView = {};
+$('.template').hide();
 
 articleView.populateFilters = function() {
   $('article').each(function() {
-    // REVIEW: We can declare several variables at once and assign their values later when using let. Keep in mind that we cannot do this with const.
     let authorName, category, optionTag;
     if (!$(this).hasClass('template')) {
-      // REVIEW: We need to take every author name from the page, and make it an option in the Author filter.
-      // To do so, Build an <option> DOM element that we can append to the author <select> element.
-      // Start by grabbing the author's name from `this` article element, and then use that bit of text to create the option tag (in a variable named `optionTag`) that we can append to the #author-filter select element.
       authorName = $(this).attr('data-author');
-
-      // DONE
       optionTag = `<option value="${authorName}">${authorName}</option>`;
 
       if ($(`#author-filter option[value="${authorName}"]`).length === 0) {
         $('#author-filter').append(optionTag);
       }
-
-      // REVIEW: Similar to the above, but...
-      // Avoid duplicates! We don't want to append the category name if the <select> already has this category as an option!
       category = $(this).attr('data-category');
 
-    
       optionTag = `<option value="${category}">${category}</option>`;
 
       if ($('#category-filter option[value="' + category + '"]').length === 0) {
@@ -36,13 +26,11 @@ articleView.populateFilters = function() {
 
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
-    
     if ($(this).val()) {
       $('article').hide();
       console.log('this', $(this).val());
       $(`article[data-author="${$(this).val()}"]`).show();
     } else {
-    
       $('article').show();
       $('article.template').hide();
     }
@@ -69,7 +57,6 @@ articleView.handleCategoryFilter = function() {
 };
 
 articleView.handleMainNav = function() {
-
   $('.tab').on('click', function () {
     console.log('this', $(this).data('content'));
     $('.tab-content').hide();
@@ -79,7 +66,6 @@ articleView.handleMainNav = function() {
 };
 
 articleView.setTeasers = function() {
-  // REVIEW: Hide elements beyond the first 2 in any article body.
   $('.article-body *:nth-of-type(n+2)').hide();
   $('#articles').on('click', '.read-on', function (e) {
     e.preventDefault();
@@ -89,7 +75,6 @@ articleView.setTeasers = function() {
 
 };
 
-// TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
